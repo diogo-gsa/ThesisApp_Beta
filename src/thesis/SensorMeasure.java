e0d387c5cea5358fdd75b06f41e3eeb1fbc470fb
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2014-03-16 15:54:34 +0000

    more changes on parsing a JSON object to a Java Object

--- src/thesis/SensorMeasure.java
+++ src/thesis/SensorMeasure.java
@@ -14,9 +14,9 @@ public class SensorMeasure {
     private String id;
     private String name;
     private long ts;
-    private Phase phaseOne;
-    private Phase phaseTwo;
-    private Phase phaseThree;
+    private Phase phase1;
+    private Phase phase2;
+    private Phase phase3;
 
     public static void main(String[] args){
         
@@ -44,9 +44,10 @@ public class SensorMeasure {
         
         //System.out.println(test);
         JSONParser parser = new JSONParser();
+        SensorMeasure measure = new SensorMeasure(test);
         
-        try {        
-            JSONObject measureJSONobject = (JSONObject) parser.parse(test);
+        try {                             
+            /*
             String id = (String)  measureJSONobject.get("id");
             String name = (String)  measureJSONobject.get("name");
             long ts = (long)  measureJSONobject.get("timestamp");
@@ -60,10 +61,19 @@ public class SensorMeasure {
             
             long voltagePhaseOne = (long) phaseOneJSONobject.get("voltage");
             System.out.println("v: "+voltagePhaseOne);
-            
+            */
             //if(phaseOneJSONobject == null) { System.out.println("=(");} else {System.out.println("=D");}
-            
 
+            JSONObject measureJSONobject = (JSONObject) parser.parse(test);
+            String id = (String)  measureJSONobject.get("id");
+            String name = (String)  measureJSONobject.get("name");
+            long ts = (long)  measureJSONobject.get("timestamp");
+            Phase phase1 = measure.parsePhaseJSONobject(measureJSONobject, "1");
+            Phase phase2 = measure.parsePhaseJSONobject(measureJSONobject, "2");
+            Phase phase3 = measure.parsePhaseJSONobject(measureJSONobject, "3");
+
+            
+            
             
         } catch (ParseException e) {
             e.printStackTrace();
@@ -72,51 +82,57 @@ public class SensorMeasure {
         
     }
     
-    
-    private 
-    
 
-    public SensorMeasure(String jsonString){
-        
+    public SensorMeasure(String measureJSON){
         JSONParser parser = new JSONParser();
-        
-        
-        
-        
+        try {
+            JSONObject measureJSONobject = (JSONObject) parser.parse(measureJSON);            
+            id = (String)  measureJSONobject.get("id");
+            name = (String)  measureJSONobject.get("name");
+            ts = (long)  measureJSONobject.get("timestamp");
+            phase1 = parsePhaseJSONobject(measureJSONobject, "1");
+            phase2 = parsePhaseJSONobject(measureJSONobject, "2");
+            phase3 = parsePhaseJSONobject(measureJSONobject, "3");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
+    private Phase parsePhaseJSONobject(JSONObject obj, String phaseNumber){
+        JSONObject phases = (JSONObject) obj.get((String) "phases");
+        JSONObject specificPhase = (JSONObject) phases.get(phaseNumber);       
+        Phase result = new Phase((long)specificPhase.get("voltage"), (double)specificPhase.get("current"), (double)specificPhase.get("powerfactor"));
+        return result;
+    }
 
     private class Phase {
-        private int voltage;
-        private float current;
-        private float powerFactor;
+        private long voltage;
+        private double current;
+        private double powerFactor;
 
-        public Phase(int v, int c, int pf) {
+        public Phase(long v, double c, double pf) {
             voltage = v;
             current = c;
             powerFactor = pf;
         }
 
-        public int getVoltage() {
+        public long getVoltage() {
             return voltage;
         }
 
-
-        public float getCurrent() {
+        public double getCurrent() {
             return current;
         }
 
-        public float getPowerFactor() {
+        public double getPowerFactor() {
             return powerFactor;
         }
+    
+        @Override
+        public String toString(){
+            return " voltage: "+voltage+" | current: "+current+" | powerfactor: "+powerFactor;
+        }
+    
     }
 
 }
-
-
-/*
- * Measure Example { "id": "C8:A0:30:AC:3F:AA", "name": "Núcleo 14", "timestamp":
- * 1394897303, "phases": { "1": { "voltage": 233, "current": 0.4, "powerfactor": 0.18 },
- * "2": { "voltage": 235, "current": 0.5, "powerfactor": 0.70 }, "3": { "voltage": 240,
- * "current": 0.7, "powerfactor": 0.55 } } }
- */
